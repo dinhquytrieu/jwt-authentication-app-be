4e79fddaddc4868eb1b995bf976dfbb0af6f7e91
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-11-12 00:52:51 +0700

    chore: update baseURL fe hosting

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: 'https://user-registration-app-chi.vercel.app', // frontend address
-    origin: 'http://localhost:5173', // frontend address
+    origin: 'https://jwt-authentication-app-fe.vercel.app', // frontend address
+    // origin: 'http://localhost:5173', // frontend address
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(3000);
